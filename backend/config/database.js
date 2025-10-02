4e6bf929d1e66542d01a4baacf444fc67818e7bb
felipehsss <felipehsacchi@gmail.com>
2025-10-02 16:17:44 -0300

    CONFIG

---
+++ backend/config/database.js
@@ -0,0 +1,81 @@
+import 'mysql2' from 'mysql2/promise';
+
+const pool = mysql2.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'qg_db',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+async function getConnection() {
+  return await pool.getConnection();
+}
+
+async function readAll(table, where = null) {
+  const connection = await getConnection();
+  try {
+    let sql = `SELECT * FROM ${table}`;
+    if (where) {
+      sql += ` WHERE ${where}`;
+    }
+
+    const [rows] = await connection.execute(sql);
+    return rows;
+  } finally {
+    connection.release();
+  }
+}
+
+async function read(table, where) {
+  const connection = await getConnection();
+  try {
+    let sql = `SELECT * FROM ${table}`;
+    if (where) {
+      sql += ` WHERE ${where}`;
+    }
+
+    const [rows] = await connection.execute(sql);
+    return rows[0] || null;
+  } finally {
+    connection.release();
+  }
+}
+
+async function create(table, data) {
+  // Obtém uma conexão com o banco de dados
+  const connection = await getConnection();
+  try {
+    const columns = Object.keys(data).join(', ');
+    const placeholders = Array(Object.keys(data).length).fill('?').join(', ');
+    const sql = `INSERT INTO ${table} (${columns}) VALUES (${placeholders})`;
+    const values = Object.values(data);
+    const [result] = await connection.execute(sql, values);
+    return result.insertId;
+  } finally {
+
+    connection.release();
+  }
+}
+
+async function update(table, data, where) {
+    const connection = await getConnection();
+    try {
+        const set = Object.keys(data)
+            .map(column => `${column} = ?`)
+            .join(', ');
+
+        const sql = `UPDATE ${table} SET ${set} WHERE ${where}`;
+        const values = Object.values(data);
+
+        const [result] = await connection.execute(sql, [...values]);
+        return result.affectedRows;
+    } finally {
+        connection.release();
+    }
+}
+
+
+export { create, readAll, read, update };
