d23c08e4917de698638449a26d4b1dd1ea96cca9
felipehsss <felipehsacchi@gmail.com>
2025-11-22 16:21:18 -0300

    db

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port:  3306,
+  port: process.env.DB || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
