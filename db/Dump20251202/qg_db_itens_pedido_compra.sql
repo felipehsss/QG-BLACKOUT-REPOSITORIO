f26b2e93c071dafe9728c774ee47000bfca024b3
felipehsss <felipehsacchi@gmail.com>
2025-12-02 12:05:35 -0300

    db

---
+++ db/Dump20251202/qg_db_itens_pedido_compra.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE  IF NOT EXISTS `qg_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `qg_db`;
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: qg_db
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `itens_pedido_compra`
+--
+
+DROP TABLE IF EXISTS `itens_pedido_compra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `itens_pedido_compra` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `pedido_compra_id` int(11) NOT NULL,
+  `produto_id` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `custo_unitario` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `pedido_compra_id` (`pedido_compra_id`),
+  KEY `produto_id` (`produto_id`),
+  CONSTRAINT `itens_pedido_compra_ibfk_1` FOREIGN KEY (`pedido_compra_id`) REFERENCES `pedidos_compra` (`pedido_compra_id`) ON DELETE CASCADE,
+  CONSTRAINT `itens_pedido_compra_ibfk_2` FOREIGN KEY (`produto_id`) REFERENCES `produtos` (`produto_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `itens_pedido_compra`
+--
+
+LOCK TABLES `itens_pedido_compra` WRITE;
+/*!40000 ALTER TABLE `itens_pedido_compra` DISABLE KEYS */;
+INSERT INTO `itens_pedido_compra` VALUES (1,1,4,10,20.00),(2,2,20,10,20.00),(3,3,2,20,20.00),(4,4,20,1,20.00);
+/*!40000 ALTER TABLE `itens_pedido_compra` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-02 12:04:50
