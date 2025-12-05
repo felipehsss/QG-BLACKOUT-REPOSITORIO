8d547dadcce6a9dbdbd6337b9076d9fdb0252504
felipehsss <felipehsacchi@gmail.com>
2025-12-05 01:36:04 -0300

    logo

--- frontend-qgb/src/app/login/page.jsx
+++ frontend-qgb/src/app/login/page.jsx
@@ -5,14 +5,18 @@ import { LoginForm } from "@/components/login-form"
 export default function LoginPage() {
   return (
     <div
-      className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10"  style={{
+        backgroundImage: "url('/background.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}>
+      
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div
-            className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
+         <div className="rounded-lg p-2">
+            <img src="/logo/2.svg" alt="QG-BLACKOUT Logo" className="w-auto"/>
           </div>
-          Acme Inc.
+        
         </a>
         <LoginForm />
       </div>
